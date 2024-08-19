2898f3d480770b848a4d11d8cec13223d19ff6a1
Jonathan James <alliterated.arithmancer@gmail.com>
2024-08-19 00:37:43 +0000

    Install aws cli

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,8 +3,8 @@
 declare -gr DevContainerDir="$(cd "$(dirname "$0")"; pwd)"
 declare -gr RootDir="$(dirname "$DevContainerDir")"
 
-post_create() {
-  echo "post_create"
+setup_ruby() {
+  echo 'setup_ruby'
   pushd "$RootDir"
   sudo gem update --system 3.5.17
   gem install bundler
@@ -12,12 +12,36 @@ post_create() {
   popd
 }
 
+install_aws_cli() {
+  echo 'install_aws_cli'
+  if [ "$(uname -m)" = 'x86_64' ]; then
+    local -r url='https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip'
+  else
+    local -r url='https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip'
+  fi
+  local -r filename='awscliv2.zip'
+
+  pushd '/tmp'
+  curl "$url" -o "$filename"
+  unzip "$filename"
+  rm "$filename"
+  sudo ./aws/install
+  rm -rf 'aws'
+  popd
+}
+
+post_create() {
+  echo 'post_create'
+  setup_ruby
+  install_aws_cli
+}
+
 post_start() {
-  echo "post_start"
+  echo 'post_start'
 }
 
 post_attach() {
-  echo "post_attach"
+  echo 'post_attach'
 }
 
 setup() {
